67587da5d322080cd244202b116daead23d36943
ADSFAaron <aaron881015@hotmail.com.tw>
2020-12-15 17:39:14 +0800

    ADSF 12/15 17:40 Commit

--- app/src/main/java/com/example/conference_infinity/Activity_Setting_Preference.java
+++ app/src/main/java/com/example/conference_infinity/Activity_Setting_Preference.java
@@ -31,7 +31,8 @@ public class Activity_Setting_Preference extends AppCompatActivity {
         themeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(Activity_Setting_Preference.this, Activity_Setting_Theme.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/conference_infinity/Activity_Setting_Theme.java
+++ app/src/main/java/com/example/conference_infinity/Activity_Setting_Theme.java
@@ -1,14 +1,123 @@
 package com.example.conference_infinity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatRadioButton;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import at.markushi.ui.CircleButton;
 
 public class Activity_Setting_Theme extends AppCompatActivity {
+    AppCompatRadioButton light_btn, dark_btn;
+    CircleButton back_btn;
+    GlobalVariable user;
+    boolean change = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_theme);
+
+        light_btn = findViewById(R.id.setting_theme_light_btn);
+        dark_btn = findViewById(R.id.setting_theme_dark_btn);
+        back_btn = findViewById(R.id.setting_theme_back_btn);
+
+        user = (GlobalVariable) getApplicationContext();
+
+        // setting button
+        if (user.preferThemeCode.equals(user.Language[0])) {
+            // light theme
+            light_btn.setChecked(true);
+            dark_btn.setChecked(false);
+        } else if (user.preferThemeCode.equals(user.Language[1])) {
+            // dark theme
+            light_btn.setChecked(false);
+            dark_btn.setChecked(true);
+        }
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void onRadioButtonClick(View view) {
+        boolean isSelected = ((AppCompatRadioButton) view).isChecked();
+        change = true;
+        switch (view.getId()) {
+            case R.id.setting_theme_light_btn:
+                if (isSelected) {
+                    user.preferThemeCode = user.Theme[0];
+//                    light_btn.setTextColor(getColor(R.color.white));
+//                    dark_btn.setTextColor(getColor(R.color.dark_gray));
+                }
+                break;
+            case R.id.setting_theme_dark_btn:
+                if (isSelected) {
+                    user.preferThemeCode = user.Theme[1];
+//                    light_btn.setTextColor(getColor(R.color.dark_gray));
+//                    dark_btn.setTextColor(getColor(R.color.white));
+                }
+                break;
+        }
+    }
+
+    void UploadPrefer(boolean theme, boolean lang) {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        if (theme) {
+            db.collection("Preference")
+                    .document(user.userEmail)
+                    .update("Theme", user.preferThemeCode)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TAG", "Error writing document", e);
+                        }
+                    });
+        }
+        if (lang) {
+            db.collection("Preference")
+                    .document(user.userEmail)
+                    .update("Language", user.preferThemeCode)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TAG", "Error writing document", e);
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // if theme has change
+        if (change) {
+            UploadPrefer(true, false);
+        }
     }
 }
